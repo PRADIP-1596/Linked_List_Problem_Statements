084a4faf028fe0d24f228505c9566a85c4c5ed96
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-06-25 23:51:55 +0530

    "[Pradip]add:Delete The last elements in the linked list"

--- LinkedListProblemStatements/src/com/bridgelabz/Linkedlist/INode.java
+++ LinkedListProblemStatements/src/com/bridgelabz/Linkedlist/INode.java
@@ -1,5 +1,10 @@
 package com.bridgelabz.Linkedlist;
 
-public interface INode {
 
+public interface INode<K> {
+    void setKey(K key);
+    K getKey();
+
+    void setNext(INode<K> next);
+    INode<K> getNext();
 }
--- LinkedListProblemStatements/src/com/bridgelabz/Linkedlist/LinkedListMain.java
+++ LinkedListProblemStatements/src/com/bridgelabz/Linkedlist/LinkedListMain.java
@@ -2,4 +2,19 @@ package com.bridgelabz.Linkedlist;
 
 public class LinkedListMain {
 
+	public static void main(String[] args) {
+		LinkedListOperations operations = new LinkedListOperations();
+		
+		Node<Integer> firstNode = new Node<>(56);
+	    Node<Integer> secondNode = new Node<>(30);
+	    Node<Integer> thirdNode = new Node<>(70);
+
+	    operations.addNode(firstNode);
+	    operations.addNode(thirdNode);
+	    
+	    operations.addNodeInMiddle(secondNode);
+	    
+	    operations.displayNode();
+	  }
+
 }
